86cdf093bfe7ab3a163aa05fcc3185e03e2ad8ed
Zach Boston <wyb5@txstate.edu>
2025-12-15 15:40:57 -0600

    Mobile layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
-      >
+<body
+  className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black overflow-x-hidden`}
+>
         <Navbar />
         {children}
       </body>
